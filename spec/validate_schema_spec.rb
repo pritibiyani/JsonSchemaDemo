8f0a995515cdca2ebda72ddd07da0cac6f0fa0e1
priti <priti.biyani6@gmail.com>
2016-10-25 14:51:21 +0530

    No need of reading schema. If passed filename relative to project root, it works

--- spec/validate_schema_spec.rb
+++ spec/validate_schema_spec.rb
@@ -3,22 +3,24 @@ require 'json'
 require_relative '../src/response_validator'
 
 describe ResponseValidator do
+	SCHEMA_FILE = 'schema/book_schema.json'
+
 	it 'should return true for correct book response' do
 		response_validator = ResponseValidator.new(read_resource('book.json'))
-		response  = response_validator.validate_response_for_json_document(read_schema('book_schema.json'))
+		response  = response_validator.validate_response_for_json_document(SCHEMA_FILE)
 		expect(response[:status]).to eq true
 	end
 
 	it 'should return false when mandatory fields are missing' do
 		response_validator = ResponseValidator.new(read_resource('book_mandatory_filed_missing.json'))
-		response  = response_validator.validate_response_for_json_document(read_schema('book_schema.json'))
+		response  = response_validator.validate_response_for_json_document(SCHEMA_FILE)
 		expect(response[:status]).to eq false
 		puts "Failed: #{response[:errors]}"
 	end
 
 	it 'should return true when only mandatory fields are presenr' do
 		response_validator = ResponseValidator.new(read_resource('book_only_mandatory_fields_present.json'))
-		response  = response_validator.validate_response_for_json_document(read_schema('book_schema.json'))
+		response  = response_validator.validate_response_for_json_document(SCHEMA_FILE)
 		expect(response[:status]).to eq true
 	end
 
@@ -28,10 +30,5 @@ describe ResponseValidator do
 		contents
 	end
 
-	def read_schema(schema_file)
-		file_path = File.expand_path(File.dirname(__FILE__) + "/../schema/#{schema_file}")
-		contents  = JSON.parse(File.read(file_path))
-		contents
-	end
 end
 
